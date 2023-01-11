5d4f837e18429efd45a0ed5e851bbcfe70acbb55
sandrumirceaioan <mircea.sandru@gosocialdev.eu>
2023-01-11 16:35:53 +0200

    env update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BACKEND_URL: 'https://lectoform-api.herokuapp.com/api',
-  BACKEND_APP: 'https://lectoform-api.herokuapp.com'
+  BACKEND_URL: 'http://api.bidbro.ro/api',
+  BACKEND_APP: 'http://api.bidbro.ro'
 };

941444082dbe987e3e873a67d573841ec9d1758f
sandrumirceaioan <mircea.sandru@gosocialdev.eu>
2023-01-11 23:55:15 +0200

    server from heroku config

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
 const express = require('express');
 const app = express();
 
-app.use(requireHTTPS);
 app.use(express.static('./dist/lecto-admin'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/lecto-admin/'}),
 );
 
-console.log(`Node Express server listening on http://localhost:${process.env.PORT}`);
-app.listen(process.env.PORT || 8080);
-
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+});
